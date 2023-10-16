15f61d780e97c4abb8d5f79a98ae1fe2396fa8f3
Jau Italo <jau.batista@dcx.ufpb.br>
2023-10-15 21:37:41 -0300

    rota home

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "../Pages/Login"
 import { Register } from "../Pages/Register";
+import {Home} from "../Pages/pagesMain/Home"
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
